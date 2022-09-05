4262551e31d98998abb9703b550cfd3dba440888
user1026 <2914153618@qq.com>
2022-09-05 16:45:14 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import { useState } from 'react'
 import {Div} from "./AppCss";
 import routes from "@/router/router.jsx" 
-import {Routes,Route,Router,useRoutes} from "react-router-dom"
+
 import HOCRouter from "@/router/HOCRouter.jsx";
-import Login from "./view/baseHtml/login/login.jsx";
-import Undefined from "./view/baseHtml/404/404.jsx";
+
 function App() {
 
   const element = useRoutes(routes)
   return (
     <Div>
-        <Routes>
-          <Route path='/login' element={<Login />}></Route>
+       
+     
           <HOCRouter RouterList={Routes}></HOCRouter>
-          <Route path="*" element={<Undefined />}></Route>
-        </Routes>
+         
+       
     </Div>
   )
 }
--- src/router/HOCRouter.jsx
+++ src/router/HOCRouter.jsx
@@ -1,15 +1,32 @@
 import {Route} from "react-router-dom"
-
-
+import NotFinded from "@/view/baseHtml/404/404.jsx";
+import Login from "@/view/baseHtml/login/login.jsx";
+import {Routes,Route,Router,useRoutes} from "react-router-dom"
 export default function HOCRouter (props){
 const {RouterList}=props;
-    if(RouterList.length===0){
+const RouterMap=(List)=>{
+    let arr=[];
+    for(let i=0;i<List.length;i++){
+        List[i].element=import(`@/view/${list[i].element}`);
+        if(List[i].children){
+            List[i].children=RouterMap(List[i].children);
+        }
+    }
+   return arr;
+}
+RouterList=RouterList.map(val)
+    if(RouterList.length!==0){
             return (
-                <div></div>
+              <Routes>
+               {useRoutes(RouterList)}
+              </Routes>
             )
     }else{
         return (
-            <Route></Route>
+            <Routes>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="*" element={<NotFinded />}></Route>
+            </Routes>
         )
     }
 
